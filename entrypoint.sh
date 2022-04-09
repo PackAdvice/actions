266ffe33a456b83282286ef53d19f66dd6cfebea
sya-ri <git@s7a.dev>
2022-04-09 10:39:30 +0900

    fix: Change option name

--- entrypoint.sh
+++ entrypoint.sh
@@ -62,7 +62,7 @@ echo '::group::PackAdvice version'
 echo '::endgroup::'
 
 echo "::group::PackAdvice output"
-./packadvice --export "$EXPORT_FILE_NAME" "$GITHUB_WORKSPACE/$INPUT_PATH" 2>&1
+./packadvice --output "$EXPORT_FILE_NAME" "$GITHUB_WORKSPACE/$INPUT_PATH" 2>&1
 echo '::endgroup::'
 
 if [ "$INPUT_COMMENT" = 'true' ]; then
